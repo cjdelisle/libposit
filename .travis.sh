201f53281f430bcf02e5e807a143f8a5b32a2aa9
Caleb James DeLisle <cjd@cjdns.fr>
2019-05-13 22:01:47 +0200

    travis: Try to fix osx

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,7 @@
 die() { exit 1; }
 eval "${MATRIX_EVAL}";
 if test "`uname`" = "Darwin"; then
-    brew install mpfr || die
+    brew install mpfr || echo 'install mpfr failed, continuing anyway...'
     export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:`echo /usr/local/Cellar/mpfr/*/lib/pkgconfig`"
     ./configure || die
     make || die
